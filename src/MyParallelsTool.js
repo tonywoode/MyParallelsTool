c16c3294dd2e543d1e5a36324fd5cdc3fe248748
tony woode <tonywoodecode@gmail.com>
2017-07-10 20:06:18 +0100

    keep checking until files aren't open

--- src/MyParallelsTool.js
+++ src/MyParallelsTool.js
@@ -4,23 +4,37 @@
 
 const windowsPartition = `/Volumes/Untitled` 
 const parallelsVM = `/Users/twoode/CODE/VMs/Parallels/My Boot Camp.pvm/My Boot Camp.app`
-
-
 const spawn = require( 'child_process' ).spawn
-console.log( `checking you've not left anything open on ${windowsPartition}...`)
-const ls = spawn( 'lsof', [ windowsPartition  ] )
 
-ls.stdout.on( 'data', data => console.log( `stdout: ${data}` ) )
-ls.stderr.on( 'data', data => console.log( `stderr: ${data}` ) )
+function checkForOpenFiles(callback) {
+  console.log( `checking you've not left anything open on ${windowsPartition}...`)
+  const ls = spawn( 'lsof', [ windowsPartition  ] )
 
-/*
- * strangely for lsof, return code 1 means "i didn't find anything". To not find anything is an error
- * https://stackoverflow.com/a/29843137/3536094
- */ 
-ls.on( 'close', code => {
-    console.log( `child process exited with code ${code}` )
-    if (code===1) {
+  ls.stdout.on( 'data', data => console.log( `OOPS!!!! These are still open:\n ${data}` ) )
+  ls.stderr.on( 'data', data => console.log( `Some error happened:\n ${data}` ) )
+
+  /*
+   * strangely for lsof, return code 1 means "i didn't find anything". To not find anything is an error
+   * https://stackoverflow.com/a/29843137/3536094
+   */ 
+  ls.on( 'close', exitCode => callback(exitCode))
+}
+
+function doneChecking() {
+
+  checkForOpenFiles( exitCode => {
+    if (exitCode === 1) {
       console.log(`Nothing is open, starting up...`)
-      spawn( 'open', ['-a',  parallelsVM ] ) 
+      spawn( 'open', ['-a',  parallelsVM ] )
     }
-})
+
+    if (exitCode === 0) {
+      console.log("Close the files, checking again....")
+      doneChecking()
+    }
+
+  })
+
+}
+
+doneChecking()
